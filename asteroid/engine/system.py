08f79c758ef4952ab434e93291073db7d738b165
mpariente <pariente.mnl@gmail.com>
2020-08-19 10:20:53 +0200

    [src] Adapt to new optimizer_step signatude

--- asteroid/engine/system.py
+++ asteroid/engine/system.py
@@ -114,18 +114,11 @@ class System(pl.LightningModule):
         tensorboard_logs = {"train_loss": loss}
         return {"loss": loss, "log": tensorboard_logs}
 
-    def optimizer_step(
-        self,
-        epoch: int,
-        batch_idx: int,
-        optimizer: Optimizer,
-        optimizer_idx: int,
-        second_order_closure: Optional[Callable] = None,
-    ) -> None:
+    def optimizer_step(self, *args, **kwargs):
         for sched in self.scheduler:
             if isinstance(sched, dict) and sched["interval"] == "batch":
                 sched["scheduler"].step()  # call step on each batch scheduler
-        self.optimizer.step()
+        super().optimizer_step(*args, **kwargs)
 
     def validation_step(self, batch, batch_nb):
         """ Need to overwrite PL validation_step to do validation.
